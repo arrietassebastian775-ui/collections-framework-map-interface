257bf9e62e67712007143b9376eca631abc7029e
arrietassebastian775-ui <arrietassebastian775@gmail.com>
2026-05-07 10:07:04 +0200

    uso del super en la generisidad

--- collections_framework_map_interface/src/main/java/com/example/App.java
+++ collections_framework_map_interface/src/main/java/com/example/App.java
@@ -73,7 +73,7 @@ public class App {
 		
 		// Creacion de listado de empleados
 		
-		List<? extends Persona> listadoGenerico = null;
+		List<? super Persona> listadoGenerico = Arrays.asList();
 		
 		Empleado emp1 = Empleado.builder()
 				.nombre("Jorge Francisco")
@@ -196,18 +196,30 @@ public class App {
 				.fechaAltaFacultad(LocalDate.of(2020, Month.JANUARY, 6))
 				.build();
 		
-		listadoGenerico = Arrays.asList
-				(emp1,
-				 emp2,
-				 emp3,
-				 emp4,
-				 emp5,
-				 emp6,
-				 emp7,
-				 emp8,
-				 emp9,
-				 emp10,
-				 estudiante1);
+//		listadoGenerico = Arrays.asList
+//				(emp1,
+//				 emp2,
+//				 emp3,
+//				 emp4,
+//				 emp5,
+//				 emp6,
+//				 emp7,
+//				 emp8,
+//				 emp9,
+//				 emp10,
+//				 estudiante1);
+		
+		listadoGenerico.add(emp1);
+		listadoGenerico.add(emp2);
+		listadoGenerico.add(emp3);
+		listadoGenerico.add(emp4);
+		listadoGenerico.add(emp5);
+		listadoGenerico.add(emp6);
+		listadoGenerico.add(emp7);
+		listadoGenerico.add(emp8);
+		listadoGenerico.add(emp9);
+		listadoGenerico.add(emp10);
+		listadoGenerico.add(estudiante1);
 		
 		//Crear una coleccion que agrupe empleados por genero
 //		Map<Genero, List<Empleado>> empleadosPorGenero = empleados.stream()
